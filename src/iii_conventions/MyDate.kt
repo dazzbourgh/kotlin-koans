cc7857e3844d63e3c8f9d9761ea07cc8070d3490
dazzbourgh <dazzbourgh@gmail.com>
2018-01-08 22:57:18 +0300

    25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,23 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            year > other.year -> 1
+            year < other.year -> -1
+            month > other.month -> 1
+            month < other.month -> -1
+            dayOfMonth > other.dayOfMonth -> 1
+            dayOfMonth < other.dayOfMonth -> -1
+            else -> 0
+        }
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = {
+    val self: DateRange = DateRange(this, other)
+    return self
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +25,8 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate> {
+    override fun contains(value: MyDate): Boolean {
+        return value in start..endInclusive
+    }
+}
